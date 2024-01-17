b34a30047bc777db9e98b2c6fa8a99b4bcc673d3
danielAlex92 <daniel45675@gmail.com>
2024-01-17 14:33:05 -0500

    Inicializar carpetas Backend y Frontend con DjangoRestFramework y React_Js instalados

---
+++ Codigo/Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p>Home</p>
+      </div>
+    </>
+  )
+}
+
+export default App
