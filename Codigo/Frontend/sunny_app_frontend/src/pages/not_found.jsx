89223e96817ff8a59292c88ba5cb021017519e63
Fabián Espitia <fabianespitia370@gmail.com>
2024-01-18 01:24:32 -0500

    not found page enhanced

--- Codigo/Frontend/sunny_app_frontend/src/pages/not_found.jsx
+++ Codigo/Frontend/sunny_app_frontend/src/pages/not_found.jsx
@@ -5,6 +5,7 @@ export function NotFound(){
     return (
         <main className="flex flex-col items-center justify-center h-screen">
             <a href="/" className="font-bold text-7xl mt-9 text-yellow-500">Not <span className="text-orange-600">Found</span></a>
+            <p className="text-white text-xl mt-3"> Click on 'NOT FOUND' to go back to the Home</p>
             <NotFoundImage />
         </main>
        
